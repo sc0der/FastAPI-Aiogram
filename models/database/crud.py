a72969f2bb9d7b59913b82262c784429e1191de5
sc0der <siriusproger@gmail.com>
2021-11-06 13:37:49 +0500

    added user crud

--- models/database/crud.py
+++ models/database/crud.py
@@ -5,22 +5,7 @@ from . import models, schemas
 
 #user crud
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
 
-def get_user_by_phone(db: Session, phone: str):
-    return db.query(models.User).filter(models.User.phone == phone).first()
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
 
 #  crud city
 def get_city_byID(db:Session, city_uid):
@@ -61,3 +46,50 @@ def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+class RubricaCrud:
+    def __init__(self, db:Session):
+        self.db = db
+
+    def get_user(self, user_id: int):
+        return self.db.query(models.User).filter(models.User.id == user_id).first()
+
+    def get_rubrica_by_uid(self, rubrica_id: int):
+        return self.db.query(models.Rubrica).filter(models.Rubrica.uid == rubrica_id).first()
+
+    def get_rubrics(self, skip: int = 0, limit: int = 100):
+        return self.db.query(models.Rubrica).offset(skip).limit(limit).all()
+
+    def create_rubrica(self, rubrica):
+        if self.get_rubrica_by_uid(self.db, rubrica['uid']):
+            return "Rubric is alredy added"
+        db_rubrica = models.Rubric(name=rubrica['name'], slug=rubrica['slug'], uid=rubrica['uid'])
+        self.db.add(db_rubrica)
+        self.db.commit()
+        self.db.refresh(db_rubrica)
+        return db_rubrica
+
+class UserCrud:
+    def __init__(self, db:Session):
+        self.db = db
+
+    def get_user(self, user_id: int):
+        return self.db.query(models.User).filter(models.User.id == user_id).first()
+
+    def get_user_by_phone(self, phone: str):
+        return self.db.query(models.User).filter(models.User.phone == phone).first()
+
+    def get_users(self, skip: int = 0, limit: int = 100):
+        return self.db.query(models.User).offset(skip).limit(limit).all()
+
+    def create_user(self, user: schemas.UserCreate):
+        fake_hashed_password = user.password + "notreallyhashed"
+        db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+        self.db.add(db_user)
+        self.db.commit()
+        self.db.refresh(db_user)
+        return db_user
+
+    
+    
