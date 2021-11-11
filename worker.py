1cce22c8ae87f062eec9802c408847c422ada592
sc0der <siriusproger@gmail.com>
2021-11-11 23:54:19 +0500

    added inline buttons

--- worker.py
+++ worker.py
@@ -14,25 +14,12 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 
-count = 0
-
 bot_token = "1968503343:AAHHp5u_R0eTdFnbgUKe_gGKwcentNNcH8M"
 your_telegram_id = 563792320
 
 app = Celery('worker', broker='redis://guest@localhost//')
 app.conf.timezone = 'UTC'
 app.autodiscover_tasks()
-
-
-def SendMessage(chat_id, parse_mode, msg):
-    global count
-    count += 1
-    keyboard = json.dumps(
-        {'inline_keyboard': [[{"text": count, "callback_data": "clicked"}]]})
-    data = {'chat_id': chat_id, 'text': msg,
-            'parse_mode': parse_mode, 'reply_markup': keyboard}
-    return requests.post(url="https://api.telegram.org/bot"+bot_token+"/sendMessage", data=data).json()
-
 
 app.conf.beat_schedule = {
     "see-you-in-ten-seconds-task": {
