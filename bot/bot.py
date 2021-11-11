19e942dbd8d8297d3af127c0f9c529260d34e333
sc0der <siriusproger@gmail.com>
2021-11-12 00:03:24 +0500

    updated some

--- bot/bot.py
+++ bot/bot.py
@@ -9,13 +9,6 @@ from consts import dp, bot
 async def process_start_command(message: Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
 
-@dp.message_handler(commands=['group'])
-async def process_group_command(message: Message):
-    media = [InputMediaPhoto("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2019-honda-civic-sedan-1558453497.jpg")]
-    for photo_id in range(2):
-        media.append(InputMediaPhoto("https://auto1-homepage.prod.mp.auto1.cloud/static/optimized/orange-car-hp-right-mercedez.png", 'ёжик и котятки'))
-    await bot.send_media_group(message.from_user.id, media)
-
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!", reply_markup=greet_kb)
