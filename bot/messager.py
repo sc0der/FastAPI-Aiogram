0841389dee6bf1c849234f443fe9d678d7a87a5d
sc0der <siriusproger@gmail.com>
2021-11-14 00:59:49 +0500

    some updates

--- bot/messager.py
+++ bot/messager.py
@@ -1,4 +1,6 @@
+import json
+
 def get_msg(key):
-    with open("message.json") as file:
+    with open("messages.json") as file:
         data = json.load(file)
-        return data[key]
+        return data['messages'][key]
